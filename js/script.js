8d08997d8203e5e1de8083afd0ffd21cf28f31b2
jieun <mary022689@naver.com>
2022-04-21 18:01:57 +0900

    about

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 $(window).scroll(function(){
     let sct=$(this).scrollTop();
-    let offTop=$('#main').offset().top;
+    let offTop1=$('#main').offset().top;
 
-    if(sct>offTop-1000){
-        $('#main').addClass('on')
-        
+    if(sct>offTop1-1000){
+        $('body').addClass('on')
     }else{
-        $('#main').removeClass()
+        $('body').removeClass('on')
+    }
+    if(sct>offTop1){
+        $('.scroll').addClass('on')
+    }else{
+        $('.scroll').removeClass('on')
+    }
+
+    $('.pj-info').each(function(){
+        let offTop2=$(this).offset().top;
+    
+        if(sct>offTop2-700){
+            $(this).addClass('on');
+            $(this).siblings('.pj-main').addClass('on');
+            $(this).siblings('.pj-main').find('.pj-link>a').addClass('on');
+        }else{
+            $(this).removeClass('on');
+            $(this).siblings('.pj-main').removeClass('on');
+            $(this).siblings('.pj-main').find('.pj-link>a').removeClass('on');
+        }
+    });
+
+    let offTop3=$('#about').offset().top;
+
+    if(sct>offTop3-300){
+        $('#about').find('.tit-t').stop().animate({right: 0},1500);
+        $('#about').find('.tit-b').stop().animate({right: 0, left: 0},1500);
+    }else{
+        $('#about').find('.tit-t').stop().animate({right: '-100%'},1500);
+        $('#about').find('.tit-b').stop().animate({right: '-100%', left: '120%'},1500);
     }
 });
+
