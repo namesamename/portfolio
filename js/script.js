3994c4351e58f114194ce226ed6b934234b58473
Jieun Lee <mary022689@naver.com>
2022-04-29 02:29:09 +0900

    contact

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 $(window).scroll(function(){
     let sct=$(this).scrollTop();
-    let offTop1=$('#main').offset().top;
-    let offTop4=$('#contact').offset().top;
 
+    let offTop1=$('#main').offset().top;
     if(sct>offTop1-1000){
         $('body').addClass('on')
     }else{
@@ -12,9 +11,6 @@ $(window).scroll(function(){
         $('.scroll').addClass('on')
     }else{
         $('.scroll').removeClass('on')
-    }
-    if(sct>offTop4-200){
-        $('body').removeClass('on')
     }
 
     $('.pj-info').each(function(){
@@ -32,13 +28,23 @@ $(window).scroll(function(){
     });
 
     let offTop3=$('#about').offset().top;
-
     if(sct>offTop3-300){
         $('#about').find('.tit-t').stop().animate({right: 0},1500);
         $('#about').find('.tit-b').stop().animate({right: 0, left: 0},1500);
     }else{
         $('#about').find('.tit-t').stop().animate({right: '-100%'},1500);
         $('#about').find('.tit-b').stop().animate({right: '-100%', left: '120%'},1500);
+    }
+
+    let offTop4=$('#contact').offset().top;
+    if(sct>offTop4-200){
+        $('body').removeClass('on');
+        $('.scroll').removeClass('on');
+        $('.contact').stop().animate({left: '-5%'},1500);
+        $('.con-infos').stop().animate({right: '-5%'},1500);
+    }else{
+        $('.contact').stop().animate({left: '-100%'},1500);
+        $('.con-infos').stop().animate({right: '-100%'},1500);
     }
 });
 
